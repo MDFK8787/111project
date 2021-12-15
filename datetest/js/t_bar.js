8149232a80260accb2477ae7c100791f6f257ed9
111projrct <bombpoopoo@gmail.com>
2021-12-15 17:27:46 +0800

    Update t_bar.js

--- datetest/js/t_bar.js
+++ datetest/js/t_bar.js
@@ -1,6 +1,6 @@
 function strikedata(){
-    var date = document.getElementById('timeselect').value;
-    console.log(date)
+    var time = document.getElementById('timeselect').value;
+    console.log(time)
 
     var request = new XMLHttpRequest();
     request.open("get", "https://mdfk8787.github.io/111project/tbartest/call.json");
@@ -9,6 +9,9 @@ function strikedata(){
         if (request.status == 200) {
             var json = JSON.parse(request.responseText);
             console.log(json)
+            var date = document.getElementById('date');
+            console.log(date);
+            var len_json = Object.keys(json).length; //宣告json長度
         }
     }
           
