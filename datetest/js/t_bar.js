9107439bde307dc22eef8585dde661dcf2db417e
111projrct <bombpoopoo@gmail.com>
2021-12-15 22:00:59 +0800

    Update t_bar.js

--- datetest/js/t_bar.js
+++ datetest/js/t_bar.js
@@ -5,6 +5,9 @@ function strikedata(){
     call_strike = [];
     call_open = [];
     call_close = [];
+    put_strike = [];
+    put_open = [];
+    put_close = [];
 
     var request = new XMLHttpRequest();
     request.open("get", "https://mdfk8787.github.io/111project/tbartest/call.json");
@@ -12,9 +15,8 @@ function strikedata(){
     request.onload = function () {
         if (request.status == 200) {
             var json = JSON.parse(request.responseText);
-            console.log(json)
             var date = document.getElementById('date');
-            console.log(date.value);
+            //console.log(date.value);
             var len_json = Object.keys(json).length; //宣告json長度
 
             call_strike.length = 0;//清除舊資料
@@ -39,8 +41,9 @@ function strikedata(){
             console.log(call_strike);
             console.log(call_open);
             console.log(call_close);
+
             var button_len = call_strike.length;
-            var tdata_len = document.getElementById('t_data').rows.length;
+            var tdata_len = document.getElementById('t_data').rows.length;//HTML上的按鈕行數長度
             var tdata = document.getElementById('t_data');
             if (tdata_len !== button_len) {//讓按鈕可以印到所有選到的資料呈現出來為止
                 
@@ -57,11 +60,11 @@ function strikedata(){
                         cell4 = newRow.insertCell(4);
 
                     
-                    cell0.innerHTML = cell0.innerHTML + "<button id='button_call_open_price_" + i.toString() + "'>" + 'Ask' + "</button>";//印出按鈕
-                    cell1.innerHTML = cell1.innerHTML + "<button id='button_call_close_price_" + i.toString() + "'>" + 'Bid' + "</button>";
+                    cell0.innerHTML = cell0.innerHTML + "<button id='button_call_open_price_" + i.toString() + "'>" + '--' + "</button>";//印出按鈕
+                    cell1.innerHTML = cell1.innerHTML + "<button id='button_call_close_price_" + i.toString() + "'>" + '--' + "</button>";
                     cell2.innerHTML = cell2.innerHTML + "<font id='strike_"+ i.toString() +"_data'>" + 'strike' + "</font>";
-                    cell3.innerHTML = cell3.innerHTML + "<button id='button_put_open_price_" + i.toString() + "'>" + 'Bid' + "</button>";
-                    cell4.innerHTML = cell4.innerHTML + "<button id='button_put_close_price_" + i.toString() + "'>" + 'Ask' + "</button>";
+                    cell3.innerHTML = cell3.innerHTML + "<button id='button_put_open_price_" + i.toString() + "'>" + '--' + "</button>";
+                    cell4.innerHTML = cell4.innerHTML + "<button id='button_put_close_price_" + i.toString() + "'>" + '--' + "</button>";
                 };
 
                 for (var i = 0; i < button_len; i++) {
@@ -79,7 +82,47 @@ function strikedata(){
             }    
         };
     }
-          
-    
+    put_t_data()
+              
+}
 
+function put_t_data(){
+
+    var request = new XMLHttpRequest();
+    request.open("get", "https://mdfk8787.github.io/111project/tbartest/put.json");
+    request.send(null);
+    request.onload = function () {
+        if (request.status == 200) {
+            var json = JSON.parse(request.responseText);
+            var date = document.getElementById('date');
+            var len_json = Object.keys(json).length;
+
+            put_strike.length = 0;//清除舊資料
+            put_open.length = 0;//清除舊資料
+            put_close.length = 0;//清除舊資料
+
+            for(i=0;i<len_json;i++){//從一個禮拜當中選出我們要的時間點
+
+                var d = json[i];
+                //console.log(a)
+                var e = d['date'];
+                var f = d['time'];
+                //console.log(b)還是有夜盤幹
+                if (e==date.value){
+                    if (f==time1){
+                        put_strike.push(d['strike']);
+                        put_open.push(d['open']);
+                        put_close.push(d['close']);
+                    };
+                };
+            };
+            console.log(put_strike);
+            console.log(put_open);
+            console.log(put_close);
+
+            var tdata_len = document.getElementById('t_data').rows.length;//HTML上的按鈕行數長度
+            var button_len = put_strike.length;
+            
+        }
+    }
 }
