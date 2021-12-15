940e422034c9c7840643baf8cf63e83ef4c2ec6c
111projrct <bombpoopoo@gmail.com>
2021-12-15 17:22:00 +0800

    Update drawpmf.js

--- datetest/js/drawpmf.js
+++ datetest/js/drawpmf.js
@@ -184,21 +184,3 @@ function showdate(){//確定按下去會執行的地方
     };
 
 }
-
-function strikedata(){
-    var date = document.getElementById('timeselect').value;
-    console.log(date)
-
-    var request = new XMLHttpRequest();
-    request.open("get", "https://mdfk8787.github.io/111project/datetest/tbardata/call.json");
-    request.send(null);
-    request.onload = function () {
-        if (request.status == 200) {
-            var json = JSON.parse(request.responseText);
-            console.log(json)
-        }
-    }
-          
-    
-
-}
