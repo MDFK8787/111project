ab06e67571e9faffa4b1733e2bae5a9750f9cd1e
111projrct <bombpoopoo@gmail.com>
2021-12-15 17:04:20 +0800

    Update t_bar.js

--- tbartest/t_bar.js
+++ tbartest/t_bar.js
@@ -2,21 +2,15 @@ function strikedata(){
     var date = document.getElementById('timeselect').value;
     console.log(date)
 
-    var url_call = 'call.json';
-    var row_len = document.getElementById("t_data").rows.length; //左邊表格長度
-    method: 'POST',
-        fetch(url_call)
-            .then(function (resp) {
-                return resp.json();
-            })
-            .then(function (data) {
-
-                var data_call = data; //完整的data_call.json
-                var row_len = document.getElementById("t_data").rows.length; //左邊表格長度
-                var data_len = Object.keys(data_call.code).length; //data_call中的code長度
-                var t_table = document.getElementById("t_data"); //左邊表格id
-                console.log(data_call)
-            })
+    var request = new XMLHttpRequest();
+    request.open("get", "https://mdfk8787.github.io/111project/tbartest/call.json");
+    request.send(null);
+    request.onload = function () {
+        if (request.status == 200) {
+            var json = JSON.parse(request.responseText);
+            console.log(json)
+        }
+    }
           
     
 
