6f53b539ea1f7906731c2a0df2dbd72c8494d7fa
111projrct <bombpoopoo@gmail.com>
2021-12-15 19:00:34 +0800

    Update t_bar.js

--- datetest/js/t_bar.js
+++ datetest/js/t_bar.js
@@ -17,11 +17,11 @@ function strikedata(){
             console.log(date.value);
             var len_json = Object.keys(json).length; //宣告json長度
 
-            call_strike.length = 0;
-            call_open.length = 0;
-            call_close.length = 0;
+            call_strike.length = 0;//清除舊資料
+            call_open.length = 0;//清除舊資料
+            call_close.length = 0;//清除舊資料
 
-            for(i=0;i<len_json;i++){
+            for(i=0;i<len_json;i++){//從一個禮拜當中選出我們要的時間點
 
                 var a = json[i];
                 //console.log(a)
@@ -36,14 +36,14 @@ function strikedata(){
                     };
                 };
             };
-            console.log(call_strike);
-            console.log(call_open);
-            console.log(call_close);
+            //console.log(call_strike);
+            //console.log(call_open);
+            //console.log(call_close);
             var button_len = call_strike.length;
             var tdata_len = document.getElementById('t_data');
-            if (tdata_len !== button_len) {
-                //先清除資料
-                for (var i = 0; i < tdata_len; i++) { //清除所有欄位							
+            if (tdata_len !== button_len) {//讓按鈕可以印到所有選到的資料呈現出來為止
+                
+                for (var i = 0; i < tdata_len; i++) { //把之前的資料清掉							
                     document.getElementById("t_data").deleteRow(0);
                 }
                 //建立表格
@@ -56,12 +56,18 @@ function strikedata(){
                         cell4 = newRow.insertCell(4);
 
                     
-                    cell0.innerHTML = cell0.innerHTML + "<button id='button_call_open_price_" + i.toString() + ">" + '-' + "</button>";
-                    cell1.innerHTML = cell1.innerHTML + "<button id='button_call_close_price_" + i.toString() + ">" + '-' + "</button>";
+                    cell0.innerHTML = cell0.innerHTML + "<button id='button_call_open_price_" + i.toString() + "'>" + '-' + "</button>";//印出按鈕
+                    cell1.innerHTML = cell1.innerHTML + "<button id='button_call_close_price_" + i.toString() + "'>" + '-' + "</button>";
                     cell2.innerHTML = cell2.innerHTML + "<font id='strike_"+ i.toString() +"_data'>" + 'strike' + "</font>";
-                    cell3.innerHTML = cell3.innerHTML + "<button id='button_put_open_price_" + i.toString() + ">" + '-' + "</button>";
-                    cell4.innerHTML = cell4.innerHTML + "<button id='button_put_close_price_" + i.toString() + ">" + '-' + "</button>";
+                    cell3.innerHTML = cell3.innerHTML + "<button id='button_put_open_price_" + i.toString() + "'>" + '-' + "</button>";
+                    cell4.innerHTML = cell4.innerHTML + "<button id='button_put_close_price_" + i.toString() + "'>" + '-' + "</button>";
                 };
+            } else {//當新選的資料要印出的按鈕數跟舊資料一樣長時,直接換數值就好
+                for (var i = 0; i < button_len; i++) {
+                    document.getElementById('bt_call_open_price_' + i.toString()).innerHTML = call_open[i];
+                    document.getElementById('bt_call_close_price_' + i.toString()).innerHTML = call_close[i];
+                    document.getElementById('strike_' + i.toString()+'_data').innerHTML = call_strike[i];
+                }
             }    
         };
     }
