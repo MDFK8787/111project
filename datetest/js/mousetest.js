94bf278cb6aa84ea3048aa2af904dde66e5b578f
111projrct <bombpoopoo@gmail.com>
2022-01-05 21:34:18 +0800

    Update mousetest.js

--- datetest/js/mousetest.js
+++ datetest/js/mousetest.js
@@ -7,5 +7,6 @@ if (a.pageX || a.pageY) {
     posX = a.clientX + document.documentElement.scrollLeft + document.body.scrollLeft;
     posY = a.clientY + document.documentElement.scrollTop + document.body.scrollTop;
 }*/
+var e =window.MouseEvent
 console.log(e.clientX);
 console.log(e.clientY);
