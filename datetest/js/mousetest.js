98e13d4f3604626cbe890537398ecb27517f51e5
111projrct <bombpoopoo@gmail.com>
2022-01-05 21:22:45 +0800

    Create mousetest.js

---
+++ datetest/js/mousetest.js
@@ -0,0 +1,11 @@
+var posX = 0, posY = 0;
+var a =  window.event;
+if (a.pageX || a.pageY) {
+    posX = a.pageX;
+    posY = aevent.pageY;
+} else if (a.clientX || a.clientY) {
+    posX = a.clientX + document.documentElement.scrollLeft + document.body.scrollLeft;
+    posY = a.clientY + document.documentElement.scrollTop + document.body.scrollTop;
+}
+console.log(posX);
+console.log(posY);
