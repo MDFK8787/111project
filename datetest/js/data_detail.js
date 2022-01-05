36c642e64e06c292b95c08fd5bbe6c296946e624
111projrct <bombpoopoo@gmail.com>
2022-01-05 19:36:28 +0800

    Update data_detail.js

--- datetest/js/data_detail.js
+++ datetest/js/data_detail.js
@@ -5,7 +5,7 @@ function show_detail(botton_id){
         var item = k + 1,
             buysell = "short",
             CP = "call",
-            strike = document.getElementById('strike_' + botton_id.name.toString()),
+            strike = document.getElementById('strike_data_' + botton_id.name.toString()),
             quote = botton_id.textContent,
             button_id = botton_id.id;
 
@@ -14,25 +14,25 @@ function show_detail(botton_id){
         var item = k + 1,
             buysell = "long",
             CP = "call",
-            strike = document.getElementById("strike_" + botton_id.name.toString()),
+            strike = document.getElementById("strike_data_" + botton_id.name.toString()),
             quote = botton_id.textContent,
-            button_id = bt_id.id;
+            button_id = botton_id.id;
 
     } else if (botton_id.id === "button_put_open_price_" + botton_id.name.toString()) {
         var item = k + 1,
             buysell = "long",
             CP = "put",
-            strike = document.getElementById("strike_" + botton_id.name.toString()).textContent,
+            strike = document.getElementById("strike_data_" + botton_id.name.toString()).textContent,
             quote = botton_id.textContent,
-            button_id = bt_id.id;
+            button_id = botton_id.id;
 
     } else if (botton_id.id === "button_put_close_price_" + botton_id.name.toString()) {//item = data_put["code"][bt_id.name],
         var item = k + 1,
             buysell = "short",
             CP = "put",
-            strike = document.getElementById("strike_" + botton_id.name.toString()).textContent,
+            strike = document.getElementById("strike_data_" + botton_id.name.toString()).textContent,
             quote = botton_id.textContent,
-            button_id = bt_id.id;
+            button_id = botton_id.id;
     }
     console.log(item);
     console.log(buysell);
