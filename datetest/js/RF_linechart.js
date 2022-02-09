3c254c87c73cfe6772961e16181b219d9144c9a6
111project <bombpoopoo@gmail.com>
2022-02-10 07:59:02 +0800

    Update RF_linechart.js

--- datetest/js/RF_linechart.js
+++ datetest/js/RF_linechart.js
@@ -108,7 +108,7 @@ function showdate(){//確定按下去會執行的地方
                 
     //var url = "https://mdfk8787.github.io/111project/datetest/json/20210930-20211006.json";
     var request = new XMLHttpRequest();
-    request.open("get", "C:/Users/User/Desktop/專題/F9/F9_output/otm2_spread100/json/RF_output_2021.json");
+    request.open("get", "https://mdfk8787.github.io/111project/datetest/js/RF_linechart.json");
     request.send(null);
     request.onload = function () {
         if (request.status == 200) {
