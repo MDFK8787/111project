134110d9646e832204d1895c31598c083314dc91
111project <bombpoopoo@gmail.com>
2022-02-10 20:54:50 +0800

    Update RF_linechart.js

--- datetest/js/RF_linechart.js
+++ datetest/js/RF_linechart.js
@@ -25,7 +25,7 @@ var plugin = {//資料點上的線
 
 var ctx2 = document.getElementById('myChartline').getContext('2d');//RF線型
 var myChartline = new Chart(ctx2, {
-    type: 'bar',
+    type: 'line',
     data: {
         labels: y,
         datasets: [{
