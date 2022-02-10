31419202750904ae1f9cbaa5433404458f35895c
111project <bombpoopoo@gmail.com>
2022-02-10 20:30:38 +0800

    Update RF_linechart.js

--- datetest/js/RF_linechart.js
+++ datetest/js/RF_linechart.js
@@ -1,6 +1,5 @@
 var x = [];
 var y = [];
-var xx = ['0'];//作為被x減去的陣列,要得出columns全部加起來=1
 var linex1 = [];//線型pmf的y軸
 
 var plugin = {//資料點上的線
@@ -90,18 +89,15 @@ function showdate(){//確定按下去會執行的地方
     var date = document.getElementById('date');
     var time = document.getElementById('time');
     var c = document.getElementById('c');
-    console.log(date.value);
-    console.log(time.value);//確認有抓到輸入的日期時間的地方
+    //console.log(date.value);
+    //console.log(time.value);//確認有抓到輸入的日期時間的地方
     //var weekArray = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri','Sat');
     date = date.value;
     date = date.replace(/-/g,'/');
-    //console.log(day);
-    //console.log(weekArray[day]);
     time = time.value;
-    var select = date+' '+time;//之後用來尋找csv列的名字的變數
+    var select = date+' '+time;//之後用來尋找csv[ 列 ]的名字的變數
     console.log(select);
-
-                
+      
     //var url = "https://mdfk8787.github.io/111project/datetest/json/20210930-20211006.json";
     var request = new XMLHttpRequest();
     request.open("get", "https://mdfk8787.github.io/111project/datetest/json/20211229.json");
@@ -113,7 +109,6 @@ function showdate(){//確定按下去會執行的地方
 
             x.length = 0;//讓上一個被輸入的圖表y資料被清除
             y.length = 0;//讓上一個被輸入的圖表x資料被清除
-            xx = [0];
 
             var len_json = Object.keys(json).length; //宣告json長度
             for (i=0;i<len_json;i++){
@@ -124,8 +119,6 @@ function showdate(){//確定按下去會執行的地方
                             
                             
                 x.push(strike2);//將得到的 列 資料放進陣列當中
-                xx.push(strike2);
-                x[i] = x[i] - xx[i];//得出pmf的機率(剩餘時間)
 
                 var prob = json[i].field1;//抓出index
                             
@@ -134,20 +127,10 @@ function showdate(){//確定按下去會執行的地方
                 y.push(prob1);
 
             };
-            linex1.length = 0;//讓上一個被輸入的圖表linex資料被清除
-
-            for (i=9;i<x.length;i++){//線型pmf
-                            
-                var xxx =(x[i]+x[i-9]+x[i-8]+x[i-7]+x[i-6]+x[i-5]+x[i-4]+x[i-3]+x[i-2]+x[i-1])/10;//剩餘時間計算公式
-                linex1.push(xxx);
-                
-            };
-            //console.log(x.length);
-            console.log(linex1);
 
             //myChart.data.label = y;//x軸
             //myChart.data.datasets[0].data = x;//y軸
-            myChartline.data.datasets[0].data = linex1;//線型y軸紅色區域
+            myChartline.data.datasets[0].data = x;//線型y軸紅色區域
             //myChart.update()//讓圖表更新
             myChartline.update()
         };
