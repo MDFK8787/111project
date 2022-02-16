b53bc6fc4458922ea964940ffc667e819c4c5f10
111project <bombpoopoo@gmail.com>
2022-02-16 17:20:48 +0800

    Update nb_test.js

--- datetest/js/nb_test.js
+++ datetest/js/nb_test.js
@@ -3,8 +3,8 @@ var scaleFactor = 10
       mean = 12,//from   w ww. de m o  2  s .  co  m
       sigma = 4;
 for(ii=0;ii<25;ii+=1) {
-   var y = gaussian(ii)
-   data.push({ii:ii,y:y*scaleFactor});
+   var jj = gaussian(ii)
+   data.push({ii:ii,jj:jj*scaleFactor});
 }
 //taken from Jason Davies science library
 // https://github.com/jasondavies/science.js/
