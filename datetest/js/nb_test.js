edae6b76fb7ea389c680644ff64b3abfb4893685
111project <bombpoopoo@gmail.com>
2022-02-16 17:15:15 +0800

    Update nb_test.js

--- datetest/js/nb_test.js
+++ datetest/js/nb_test.js
@@ -2,16 +2,16 @@ var data = [];
 var scaleFactor = 10
       mean = 12,//from   w ww. de m o  2  s .  co  m
       sigma = 4;
-for(x=0;x<25;x+=1) {
-   var y = gaussian(x)
-   data.push({x:x,y:y*scaleFactor});
+for(ii=0;ii<25;ii+=1) {
+   var y = gaussian(ii)
+   data.push({ii:ii,y:y*scaleFactor});
 }
 //taken from Jason Davies science library
 // https://github.com/jasondavies/science.js/
-function gaussian(x) {
+function gaussian(ii) {
    var gaussianConstant = 1 / Math.sqrt(2 * Math.PI);
-   x = (x - mean) / sigma;
-   return gaussianConstant * Math.exp(-.5 * x * x) / sigma;
+   ii = (ii - mean) / sigma;
+   return gaussianConstant * Math.exp(-.5 * ii * ii) / sigma;
 };
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart("myChart", {
